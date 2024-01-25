15f87a94ad6545fd7c5ebc0de030a6742f4c6669
bronivichok1 <lazarenkov01@gmail.com>
2024-01-25 14:20:27 +0300

    dev 0.0.1

---
+++ untitled/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+    <title>PHP App</title>
+  </head>
+  <body>
+
+<?php
+    echo "<b>FreeBot</b>";
+    echo "</br>Hi";
+
+    echo ""
+?>
+  </body>
+</html>
