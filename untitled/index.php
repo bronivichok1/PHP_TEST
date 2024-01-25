e572eb945207451e21651561fb8c3e19f0eb52d9
bronivichok1 <lazarenkov01@gmail.com>
2024-01-25 15:41:51 +0300

    dev 0.0.2

--- untitled/index.php
+++ untitled/index.php
@@ -11,10 +11,39 @@
   <body>
 
 <?php
+    echo abs(-22);
+    $number=-4.5;
+    $str="Work";
+    $nums=array(2,2,2,2);
+    $arr=[2,2,3.2,true,'str'];
+    $list=["age"=>50,"name"=>"lesha"];
     echo "<b>FreeBot</b>";
     echo "</br>Hi";
-
-    echo ""
+    echo "\"Hello $str\"";
+    echo ' ' . $number ."hours". $str;
+    echo "<input type='text'> FreeBot</input>";
+    if($number<0){
+        echo 'No';
+    }
+    else{
+        echo 'Yes';
+    }
+    switch($number){
+        case 4.5:
+            echo "number=4.5";
+            break;
+        case -4.5:
+            echo "number=-4.5";
+            break;
+        default:
+            echo "No((((";
+            break;
+    }
+    $i=1;
+    while($i<=10){
+        echo $i . '</br>';
+        $i++;
+    }
 ?>
   </body>
 </html>
